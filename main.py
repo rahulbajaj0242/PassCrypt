dd0c4cde6c31ddf428b1fa876a63d5d4ca84b060
Rahul Bajaj <rahulbajaj0242@gmail.com>
2022-12-25 20:24:00 -0500

    created functionality to add data in SQL

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 import psycopg2
 
 
+master_password = "secretpassword"
 
+def startup():
+  print('Welcome to your own personal Password Manager!')
+  i  = input('\n\nFirst, Please add the master password to authenticate yourself: ')
+
+  if(i == master_password):
+    return '1'
+  else:
+    return '0'
+
+def menu():
+  print('1. Add new password')
+  print('2. See all passwords')
+  print('0. Exit')
+
+  i = input('Select option from main menu (1/2/0): ')
+
+  if i == '3': exit()
+
+  return i
+
+  
 
 def main():
   
@@ -10,8 +32,19 @@ def main():
 
     cur = conn.cursor()
 
-    # cur.execute('CREATE TABLE password(id serial PRIMARY KEY, )')
+    # create a table called passwords with fields id, username, password and website
+    # cur.execute('CREATE TABLE passwords(id serial PRIMARY KEY, username varchar(255), password varchar(255), website varchar(255));')
 
+    auth = startup()
+    m = menu() if auth == '1' else print("Wrong Password! Exiting...")
+
+    if m == '1':
+      print('Please add following details: ')
+      website = input('Website Name: ')
+      username = input('Username: ')
+      password = input('Password: ')
+
+      cur.execute(f'INSERT INTO passwords(username, password, website) VALUES(%s, %s, %s);', (username, password, website))
 
 
     # Make the changes to the database persistent
