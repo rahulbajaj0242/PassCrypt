91f2ac1a95a4b58e3c243a6b3ebc5b51543b93bb
Rahul Bajaj <rahulbajaj0242@gmail.com>
2022-12-25 19:13:54 -0500

    connection working

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 import psycopg2
 
 
-try:
-
-  conn = psycopg2.connect("dbname='postgres' user='postgres' host='localhost' password='postgres' port='5432'")
 
 
-  cur = conn.cursor()
+def main():
+  
+  try:
+    conn = psycopg2.connect("dbname='postgres' user='postgres' host='localhost' password='postgres' port='5432'")
 
-# Execute a command: this creates a new table
-  cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+    cur = conn.cursor()
 
-# Pass data to fill a query placeholders and let Psycopg perform
-# the correct conversion (no more SQL injections!)
-  cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)",(100, "abc'def"))
+    # cur.execute('CREATE TABLE password(id serial PRIMARY KEY, )')
 
-# Query the database and obtain data as Python objects
-  cur.execute("SELECT * FROM test;")
-  cur.fetchone()
-  (1, 100, "abc'def")
 
-# Make the changes to the database persistent
-  conn.commit()
 
-# Close communication with the database
-  cur.close()
-  conn.close()
+    # Make the changes to the database persistent
+    conn.commit()
 
-except (Exception, psycopg2.Error) as error :
+    # Close communication with the database
+    cur.close()
+    conn.close()
+
+  except (Exception, psycopg2.Error) as error :
     print ("Error while connecting to PostgreSQL", error)
+
+
+
+
+
+
+if __name__ == '__main__':
+  main()
